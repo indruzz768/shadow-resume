4f61c786cf47c90f1844f2f88f849183567ede39
indran <indransatheesan321@gmail.com>
2025-07-02 18:05:31 +0530

    Add tailwindcss and autoprefixer to postcss config

--- theme/static_src/postcss.config.js
+++ theme/static_src/postcss.config.js
@@ -1,7 +1,9 @@
 module.exports = {
   plugins: {
-    "postcss-import": {},
-    "postcss-simple-vars": {},
-    "postcss-nested": {}
+    'postcss-import': {},
+    'postcss-simple-vars': {},
+    'postcss-nested': {},
+    'tailwindcss': {},
+    'autoprefixer': {},
   },
-}
+};
