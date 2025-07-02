73748b6b5dd569d8f94f1023dff6afd309f5cb37
indran <indransatheesan321@gmail.com>
2025-07-02 16:15:38 +0530

    Update settings for Render deployment

--- config/settings.py
+++ config/settings.py
@@ -164,9 +164,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# Ensure that Whitenoise can serve static files in production
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_DIRS = [BASE_DIR / "static", BASE_DIR / "theme" / "static"]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"), os.path.join(BASE_DIR, "theme", "static")]
 
 # Media files (user-uploaded content)
 
