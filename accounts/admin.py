3293dff3f486b6ef7be694d469af4961b409f664
indran <indransatheesan321@gmail.com>
2025-07-04 21:28:07 +0530

    Show profile_photo field in admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -4,13 +4,13 @@ from .models import User
 
 class CustomUserAdmin(UserAdmin):
     model = User
-    list_display = ['username', 'email', 'role', 'is_staff', 'is_active', 'is_superuser']
+    list_display = ['username', 'email', 'role', 'is_staff', 'is_active', 'is_superuser', 'profile_photo']
     list_filter = ('role',)
     fieldsets = UserAdmin.fieldsets + (
-        ('Role Info', {'fields': ('role',)}),
+        ('Role Info', {'fields': ('role', 'profile_photo')}),
     )
     add_fieldsets = UserAdmin.add_fieldsets + (
-        ('Role Info', {'fields': ('role',)}),
+        ('Role Info', {'fields': ('role', 'profile_photo')}),
     )
 
 admin.site.register(User, CustomUserAdmin)
